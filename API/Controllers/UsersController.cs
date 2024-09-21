32d2bed5a5eec89e4decbf49eb1abfbce013feef
lovrenovic <lovrenovic@gmail.com>
2024-09-22 00:27:37 +0200

    22092024_0030

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -6,11 +6,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
-[ApiController]
-[Route("api/[controller]")] // /api/users
-
-
-public class UsersController(DataContext context) : ControllerBase
+public class UsersController(DataContext context) : BaseApiController
 {
 
     [HttpGet]
