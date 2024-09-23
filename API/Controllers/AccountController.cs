a4057fde5eb0c2e44f4b0b1c866a3718cf74efe2
lovrenovic <lovrenovic@gmail.com>
2024-09-23 16:27:33 +0200

    23092024_1627

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,3 +1,5 @@
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
@@ -32,6 +34,28 @@ public async Task<ActionResult<AppUser>> Register (RegisterDto registerDto)
     return user;
 }
 
+[HttpPost("login")]
+
+public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+{
+var user = await context.Users.FirstOrDefaultAsync(x => 
+    x.UserName == loginDto.Username.ToLower());
+
+    if (user == null) return Unauthorized("Invalid username");
+    
+    using var hmac = new HMACSHA512(user.PasswordSalt);
+
+    var ComputedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+    for (int i=0; i < ComputedHash.Length; i++)
+    {
+
+        if (ComputedHash[i] != user.PasswordHash[i]) return Unauthorized("Invalid password");
+    }
+
+    return user;
+
+}
 private async Task<bool> UserExist(string username)
 
 {
